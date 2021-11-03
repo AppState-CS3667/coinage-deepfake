1d666ff81c78a0db22ad5207f25e75e1369c18bb
loganwrichardson <logan.richardson90@gmail.com>
2021-11-03 10:14:09 -0400

    Demo.java

--- Demo.java
+++ Demo.java
@@ -10,6 +10,15 @@ public class Demo {
 	public void main(String[] args) {
 
 	//Possibly consolidate this into makeCoin() 
+		
+	Coin USDCoin = new USDCoin();
+	Coin GBPCoin = new GBPCoin();
+
+	USDCoin.toString();
+	
+	
+	
+	
 		if(inspection()) {
 			System.out.println("The" + baseCoin + "has passed inspection.");
 		}
