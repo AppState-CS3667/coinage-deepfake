d62953ec3a87c6793916f71137f601f191d34c97
IsaacMAllen <allenim@appstate.edu>
2021-11-03 10:44:15 -0400

    added bool fields for the makecoin method

--- Coin.java
+++ Coin.java
@@ -1,5 +1,7 @@
 public abstract class Coin implements metals {
-    
+    public boolean inspect;
+    public boolean smooth;
+    private boolean
     protected String commonName;
     protected double denomination;
     protected String countryCode;
