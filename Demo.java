3bcc3f6ccbfefa05cd9f53eae40457dfe4bf9c98
loganwrichardson <logan.richardson90@gmail.com>
2021-11-03 16:44:24 -0400

    Working on Demo

--- Demo.java
+++ Demo.java
@@ -6,25 +6,28 @@ public class Demo {
 
         public void main(String[] args) {
 
-	Coin demoCoin = new demoCoin();
-	Scanner coinMaking = new Scanner(System.in);
+	//Coin demoCoin = new demoCoin();
+	//Scanner coinMaking = new Scanner(System.in);
 	
 
 	//Possibly consolidate this into makeCoin()
 	System.out.println("-----------------------------------------------------------------------------");
 	System.out.println("Welcome to Deep Fake!");
 	System.out.println("What coin would you like to make?");
-	System.out.println("Which countries coin do you want to make? Enter USD or GBP");
+	System.out.println("Which counties coin do you want to make? Enter USD or GBP:");
 	String counterfeitCoinCountry = coinMaking.nextLine();
-	for(counterfeitCoinCountry.compareTo("USD") == false) || (counterfeightCoinCountry.compareTo("GBP") == false) {
+	counterfeitCoinCountry.toUpperCase();
+	while((counterfeitCoinCountry.compareTo("USD") == false) && (counterfeitCoinCountry.compareTo("GBP") == false)) {
 		System.out.println("We can't make that type of coin. \n Try again.");
-		CounterfeightCoinCountry = coinMaking.nextLine();
+		CounterfeitCoinCountry = coinMaking.nextLine();
+		counterfeitCoinCountry.toUpperCase();
 	}
 	if(counterfeitCoinCountry.compareTo("USD") == true) {
 		System.out.println("You want to make a USD Coin.");
 		System.out.println("What denomination do you want to make?");
 	}
-	else if(counterfeightCoinCountry.compareTo("GBP") == true) {
+
+	else if(counterfeitCoinCountry.compareTo("GBP") == true) {
 		System.out.println("You want to make a GBP Coin.");
 		System.out.println("What denomination do you want to make?");
 	}
@@ -39,22 +42,23 @@ public class Demo {
 	System.out.println("This coin is composed of " + UDSCoin.getComposition());
 
         System.out.println("-----------------------------------------------------------------------------");
-	System.out.println("A GBP " + GBPCoin.getDesciption() + " 
+	System.out.println("A GBP " + GBPCoin.getDesciption() + "");
 	System.out.println(GBPCOIN);
 
-	USDCoin.toString();
+//	USDCoin.toString();
 	
-		if(inspection()) {
-			System.out.println("The" + baseCoin + "has passed inspection.");
-		}
-		if(smoothing()) {
-			System.out.println("The" + baseCoin + "has been properly smoothed.");
-		}
-		if(buffing()) {
-			System.out.println("The" + baseCoin + "has been properly buffed.");
-		}
+	//	if(inspection()) {
+	//		System.out.println("The" + baseCoin + "has passed inspection.");
+	//	}
+	//	if(smoothing()) {
+	//		System.out.println("The" + baseCoin + "has been properly smoothed.");
+	//	}
+	//	if(buffing()) {
+	//		System.out.println("The" + baseCoin + "has been properly buffed.");
+	//	}
+
+	//}
+
 
 	}
-
-
 }
