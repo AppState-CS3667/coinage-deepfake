5299d10e0f1fa3d7d3b7cce9235bbfb631bbb9b6
IsaacMAllen <allenim@appstate.edu>
2021-11-04 13:33:00 -0400

    refactored to correct method signature of calls to makeCoin

--- GBPMint.java
+++ GBPMint.java
@@ -3,63 +3,63 @@ public class GBPMint extends MintFactory {
     private Coin c;
 
     private GBPMint(double denom, String cc) {
-		rand.setSeed((int)denom * 17);
+	rand.setSeed((int)denom * 17);
     } 
- 
+
     public static GBPMint getInstance() {
+	if (uniqueMint == null) {
+	    synchronized (GBPMint.class) {
 		if (uniqueMint == null) {
-			synchronized (GBPMint.class) {
-				if (uniqueMint == null) {
-					// denomination and  countryCode will be set in the Demo class 
-					uniqueMint = new GBPMint(denomination, countryCode); 
-				}
-			}
+		    // denomination and  countryCode will be set in the Demo class 
+		    uniqueMint = new GBPMint(denomination, countryCode); 
 		}
+	    }
+	}
     }
     //To be called in Demo
-    public Coin makeCoin(double denom) {
-		switch(denom) {
-			case 0.01:
-			c = new GBPPence(0.01, "GBP", new CompPT());
-			break;
-            case 0.02:
-            c = new GBPTwoPence(0.02, "GBP", new CompPT());
-            break;
-			case 0.05:
-			c = new GBPFivePence(0.05, "GBP", new CompNFFTF());
-			break;
-			case 0.1:
-			c = new GBPTenPence(0.1, "GBP", new CompNFFTF());
-			break;
-			case 0.20:
-			c = new GBPTwentyPence(0.20, "GBP", new CompTp());
-			break;
-			case 0.5:
-			c = new GBPFiftyPence(0.5, "GBP", new CompNFFTF());
-  			break;
-			case 1:
-			c = new GBPPound(1, "GBP", new CompPo());
-			break;
-            case 2:
-            c = new GBPTwoPound(2, "GBP", new CompT());
-            break;
-            case 5:
-            c = new GBPFivePound(5, "GBP", new CompNFFTF());
-            break;
-		}
-		c.display();
-		manafacture();
-		if (!inspection()) return null;
-		System.out.println("Passed inspection!");
-		if(!smoothing()) return null;
-		System.out.println("Passed smoothing!");
-		if(!buffing()) return null;
-		System.out.println("Passed buffing!");
-		return c;
+    public Coin makeCoin(double denom, String cc) {
+	switch(denom) {
+	    case 0.01:
+		c = new GBPPence(0.01, cc, new CompPT());
+		break;
+	    case 0.02:
+		c = new GBPTwoPence(0.02, cc, new CompPT());
+		break;
+	    case 0.05:
+		c = new GBPFivePence(0.05, cc, new CompNFFTF());
+		break;
+	    case 0.1:
+		c = new GBPTenPence(0.1, cc, new CompNFFTF());
+		break;
+	    case 0.20:
+		c = new GBPTwentyPence(0.20, cc, new CompTp());
+		break;
+	    case 0.5:
+		c = new GBPFiftyPence(0.5, cc, new CompNFFTF());
+		break;
+	    case 1:
+		c = new GBPPound(1, cc, new CompPo());
+		break;
+	    case 2:
+		c = new GBPTwoPound(2, cc, new CompT());
+		break;
+	    case 5:
+		c = new GBPFivePound(5, cc, new CompNFFTF());
+		break;
+	}
+	c.display();
+	manafacture();
+	if (!inspection()) return null;
+	System.out.println("Passed inspection!");
+	if(!smoothing()) return null;
+	System.out.println("Passed smoothing!");
+	if(!buffing()) return null;
+	System.out.println("Passed buffing!");
+	return c;
     }
-	
+
     private void manafacture() {
-	    c.smelt();
+	c.smelt();
     }
     private boolean inspection() {
 	// 1/12 chance of failing inspection 
--- USDMint.java
+++ USDMint.java
@@ -3,55 +3,55 @@ public class USDMint extends MintFactory {
     private Coin c;
 
     private USDMint(double denom, String cc) {
-		rand.setSeed((int)denom * 17);
+	rand.setSeed((int)denom * 17);
     } 
     //USDMint.getInstance() should be called 
     public static USDMint getInstance() {
+	if (uniqueMint == null) {
+	    synchronized (USDMint.class) {
 		if (uniqueMint == null) {
-			synchronized (USDMint.class) {
-				if (uniqueMint == null) {
-					// denomination and  countryCode will be set in the Demo class 
-					uniqueMint = new USDMint(denomination, countryCode); 
-				}
-			}
+		    // denomination and  countryCode will be set in the Demo class 
+		    uniqueMint = new USDMint(denomination, countryCode); 
 		}
+	    }
+	}
     }
     //To be called in Demo
-    public Coin makeCoin(double denom) {
-		switch(denom) {
-			case 0.01:
-			c = new USDPenny(0.01, "USD", new CompP());
-			break;
-			case 0.05:
-			c = new USDNickel(0.05, "USD", new CompNFFTF());
-			break;
-			case 0.1:
-			c = new USDDime(0.1, "USD", new CompHQD());
-			break;
-			case 0.25:
-			c = new USDQuarter(0.25, "USD", new CompHQD());
-			break;
-			case 0.5:
-			c = new USDHalfdollar(0.5, "USD", new CompHQD());
-  			break;
-			case 1:
-			c = new USDDollar(1, "USD", new CompD());
-			break;
+    public Coin makeCoin(double denom, String cc) {
+	switch(denom) {
+	    case 0.01:
+		c = new USDPenny(0.01, cc, new CompP());
+		break;
+	    case 0.05:
+		c = new USDNickel(0.05, cc, new CompNFFTF());
+		break;
+	    case 0.1:
+		c = new USDDime(0.1, cc, new CompHQD());
+		break;
+	    case 0.25:
+		c = new USDQuarter(0.25, cc, new CompHQD());
+		break;
+	    case 0.5:
+		c = new USDHalfdollar(0.5, cc, new CompHQD());
+		break;
+	    case 1:
+		c = new USDDollar(1, cc, new CompD());
+		break;
 
-		}
-		c.display();
-		manafacture();
-		if (!inspection()) return null;
-		System.out.println("Passed inspection!");
-		if(!smoothing()) return null;
-		System.out.println("Passed smoothing!");
-		if(!buffing()) return null;
-		System.out.println("Passed buffing!");
-		return c;
+	}
+	c.display();
+	manafacture();
+	if (!inspection()) return null;
+	System.out.println("Passed inspection!");
+	if(!smoothing()) return null;
+	System.out.println("Passed smoothing!");
+	if(!buffing()) return null;
+	System.out.println("Passed buffing!");
+	return c;
     }
-	
+
     private void manafacture() {
-	    c.smelt();
+	c.smelt();
     }
     private boolean inspection() {
 	// 1/12 chance of failing inspection 
