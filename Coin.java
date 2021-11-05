9ca1d2a769b4a105f7c9ef6246619ff2c1359398
Baylor <uberskier@gmail.com>
2021-11-05 10:52:03 -0400

    Null branch

--- Coin.java
+++ Coin.java
@@ -2,7 +2,17 @@ public abstract class Coin implements smelting {
     protected double denomination;
     protected String countryCode;
     private smelting composition;
-
+    public static Coin NULL = new NullCoin();
+    private static class NullCoin extends Coin {
+        @Override
+        public void smelt() {
+            //print null behavior for smelting here
+        }
+        @Override
+        public void display() {
+            System.out.println("");
+        }
+    }
     public Coin(double denomination, String countryCode, smelting composition) {
         this.denomination = denomination;
         this.countryCode = countryCode;
