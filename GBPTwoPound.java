12a520bd6d60e7516d010aeb9b640c0b59874a2c
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 13:59:17 -0500

    Added javacdoc comments & checkstyle

--- GBPTwoPound.java
+++ GBPTwoPound.java
@@ -1,10 +1,28 @@
+/**
+ *  GBPTwoPound class
+ *  exetends coin
+ *  @author group deepFake (Baylor Matney, Isaac Allen, Logan Richardson, Ty Brucker)
+ *  @version 1.0
+ * */
 public class GBPTwoPound extends Coin {
-    
-    public GBPTwoPound(double denomination, String countryCode, smelting composition) {
-         super(denomination, countryCode, composition);
-    }
- 
-    public void display () {
-        System.out.println("Minting a GBP TwoPound!");
-    }
+
+	/*
+	 * GBPTwoPound()
+	 * @param denomination
+	 * @param countryCode
+	 * @param smelting composition
+	 * call the super constructor
+	 */    
+	public GBPTwoPound(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+
+	/*
+	 * display()
+	 * Display method for GBPTwoPound Coin
+	 * @return void
+	 */
+	public void display() {
+		System.out.println("Minting a GBP TwoPound!");
+	}
 }
