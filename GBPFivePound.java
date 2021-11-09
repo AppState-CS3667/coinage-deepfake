e9b5f763f68ab7d56b3643bf9b64222bb5c6b29d
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 13:50:39 -0500

    Added javadoc comments

--- GBPFivePound.java
+++ GBPFivePound.java
@@ -1,10 +1,30 @@
+/**
+ * class GBPFivePound
+ * GBP Five Pound coin constructor class
+ * @author group deepFake (Baylor Matney, Isaac Allen, Logan Richardson, Ty Brucker)
+ * @version 1.0
+ */
 public class GBPFivePound extends Coin {
-    
-    public GBPFivePound(double denomination, String countryCode, smelting composition) {
-         super(denomination, countryCode, composition);
-    }
- 
-    public void display () {
-        System.out.println("Minting a GBP FivePound!");
-    }
+
+	/*
+	 * GBPFivePound ()
+	 * extends coin
+	 * GBP Five pound coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 * Calls the super constructor
+	 */ 
+	public GBPFivePound(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+
+	/* display()
+	 * display method for GBPFive Pound Coin
+	 * @return void
+	 *
+	 */
+	public void display() {
+		System.out.println("Minting a GBP FivePound!");
+	}
 }
