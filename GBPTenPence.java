f3dc7f254f5af375792349ff823246c25c0ac2b3
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 13:54:42 -0500

    Added javadoc comments & checkstyle

--- GBPTenPence.java
+++ GBPTenPence.java
@@ -1,10 +1,27 @@
+/**
+ * GBPTencPence
+ * extends coin
+ * @author group deepFake (Baylor Matney, Isaac Allen, Logan Richardson, Ty Brucker)
+ * @version 1.0
+ */
 public class GBPTenPence extends Coin {
-    
-    public GBPTenPence(double denomination, String countryCode, smelting composition) {
-         super(denomination, countryCode, composition);
-    }
- 
-    public void display () {
-        System.out.println("Minting a GBP TenPence!");
-    }
+
+	/*
+	 *GBPTenPence() 
+	 @param double denomination
+	 @param String countryCode
+	 @param smeling composition
+	 */    
+	public GBPTenPence(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+
+	/*
+	 * display()
+	 * Display method for GBPTenPence coin
+	 * @return void
+	 */
+	public void display() {
+		System.out.println("Minting a GBP TenPence!");
+	}
 }
