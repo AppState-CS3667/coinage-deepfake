b1c6348672f9aad402a6f3a954f3eec760d10040
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:31:41 -0500

    indentation

--- src/GBPTwoPenceTest.java
+++ src/GBPTwoPenceTest.java
@@ -14,69 +14,69 @@ public class GBPTwoPenceTest {
 	 * Constructor for test class
 	 * @return void
 	 */
-    @Test
-        public void testConstructor() {
-            try {
-                Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
-                assertNotNull(testGBPTwoPence);
-            }
-            catch (Exception e) {
-                fail("Constructor threw exception: " + e.getMessage());
-            }
-        }
+	@Test
+		public void testConstructor() {
+			try {
+				Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
+				assertNotNull(testGBPTwoPence);
+			}
+			catch (Exception e) {
+				fail("Constructor threw exception: " + e.getMessage());
+			}
+		}
 	/*
 	 * Function to run smelting test
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Beginning smelting:\nCu Plated Steel\n"; 
-            String testOutput = null;
-            testGBPTwoPence.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testSmelt() {
+			Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Beginning smelting:\nCu Plated Steel\n"; 
+			String testOutput = null;
+			testGBPTwoPence.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run minting test
 	 * @return void
 	 */
-    @Test
-        public void display() {
-            Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Minting a GBP TwoPence!\n"; 
-            String testOutput = null;
-            testGBPTwoPence.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void display() {
+			Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Minting a GBP TwoPence!\n"; 
+			String testOutput = null;
+			testGBPTwoPence.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run test on coin denomination
 	 * @return void
 	 */
-    @Test
-        public void testgetDenomination() {
-            Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
-            Double expectedResult = 0.02;
-            Double testOutput = testGBPTwoPence.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testgetDenomination() {
+			Coin testGBPTwoPence = new GBPTwoPence(0.02,"GBP",new CompPT());
+			Double expectedResult = 0.02;
+			Double testOutput = testGBPTwoPence.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
