d2c7a28f47f6f8ae289f18c626a91fc8468eebdf
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:40:35 -0500

    indentation

--- src/USDNickelTest.java
+++ src/USDNickelTest.java
@@ -15,68 +15,68 @@ public class USDNickelTest {
 	 * @return void
 	 */
 	@Test
-        public void testConstructor() {
-            try {
-                Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
-                assertNotNull(testUSDNickel);
-            }
-            catch (Exception e) {
-                fail("Constructor threw exception: " + e.getMessage());
-            }
-        }
+		public void testConstructor() {
+			try {
+				Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
+				assertNotNull(testUSDNickel);
+			}
+			catch (Exception e) {
+				fail("Constructor threw exception: " + e.getMessage());
+			}
+		}
 	/*
 	 * Function to run smelting test
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Beginning smelting:\n75% Cu\n25% Ni\n";; 
-            String testOutput = null;
-            testUSDNickel.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testSmelt() {
+			Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Beginning smelting:\n75% Cu\n25% Ni\n";; 
+			String testOutput = null;
+			testUSDNickel.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run minting test
 	 * @return void
 	 */
-    @Test
-        public void display() {
-            Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Minting a USD Nickel!\n"; 
-            String testOutput = null;
-            testUSDNickel.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void display() {
+			Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Minting a USD Nickel!\n"; 
+			String testOutput = null;
+			testUSDNickel.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run test on coin denomination
 	 * @return void
 	 */
-    @Test
-        public void testgetDenomination() {
-            Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
-            Double expectedResult = 0.05;
-            Double testOutput = testUSDNickel.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testgetDenomination() {
+			Coin testUSDNickel = new USDNickel(0.05,"USD",new CompNFFTF());
+			Double expectedResult = 0.05;
+			Double testOutput = testUSDNickel.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
