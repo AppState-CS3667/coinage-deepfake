e227e5fc53028d625c650944bee86f73606b0712
IsaacMAllen <allenim@appstate.edu>
2021-11-10 14:52:37 -0500

    Refactored Demo to tack on looping functionality

--- Demo.java
+++ Demo.java
@@ -3,39 +3,48 @@ import java.util.Scanner;
 
 public class Demo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner coinMaking = new Scanner(System.in);
-		String counterfeitCoinCountry;
-		double counterfeitCoinDenomination;
+	Scanner coinMaking = new Scanner(System.in);
+	String counterfeitCoinCountry;
+	double counterfeitCoinDenomination;
+	boolean runOnce = false;
 
-		//Possibly consolidate this into makeCoin()
-		System.out.println("-----------------------------------------------------------------------------");
-		System.out.println("Welcome to Deep Fake!");
-		System.out.println("What coin would you like to make?");
-		System.out.println("Which countries coin do you want to make? Enter USD or GBP:");
+	//Possibly consolidate this into makeCoin()
+	System.out.println("-----------------------------------------------------------------------------");
+	System.out.println("Welcome to Deep Fake!");
+	System.out.println("What coin would you like to make?");
+	do {
+	    if (!runOnce) {
+		System.out.println("Which countries coin do you want to make? Enter USD or GBP (0 to exit):");
+		runOnce = true;
+	    } else {
+		System.out.println("Want to make another coin? Enter USD or GBP (0 to exit):");
+	    }
+	    counterfeitCoinCountry = coinMaking.nextLine();
+	    counterfeitCoinCountry = counterfeitCoinCountry.toUpperCase();
+	    while(((!counterfeitCoinCountry.equals("GBP")) && (!counterfeitCoinCountry.equals("USD"))) && (!counterfeitCoinCountry.equals("0"))) {
+		System.out.println("We can't make that type of coin. \n Try again.");
 		counterfeitCoinCountry = coinMaking.nextLine();
 		counterfeitCoinCountry = counterfeitCoinCountry.toUpperCase();
-		while((!counterfeitCoinCountry.equals("USD")) && (!counterfeitCoinCountry.equals("GBP"))) {
-			System.out.println("We can't make that type of coin. \n Try again.");
-			counterfeitCoinCountry = coinMaking.nextLine();
-			counterfeitCoinCountry = counterfeitCoinCountry.toUpperCase();
-		}
+	    }
 
-		if(counterfeitCoinCountry.equals("USD")) {
-			System.out.println("You want to make a USD Coin.");
-			System.out.println("What denomination do you want to make?");
-			counterfeitCoinDenomination = coinMaking.nextDouble();
-			USDMint USD = USDMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
-			USD.makeCoin();
-		}
-		else if(counterfeitCoinCountry.equals("GBP")) {
-			System.out.println("You want to make a GBP Coin.");
-			System.out.println("What denomination do you want to make?");
-			counterfeitCoinDenomination = coinMaking.nextDouble();
-			GBPMint GBP = GBPMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
-			GBP.makeCoin();
-		}
-	}
+	    if(counterfeitCoinCountry.equals("USD")) {
+		System.out.println("You want to make a USD Coin.");
+		System.out.println("What denomination do you want to make?");
+		counterfeitCoinDenomination = coinMaking.nextDouble();
+		USDMint USD = USDMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
+		USD.makeCoin();
+	    }
+	    else if(counterfeitCoinCountry.equals("GBP")) {
+		System.out.println("You want to make a GBP Coin.");
+		System.out.println("What denomination do you want to make?");
+		counterfeitCoinDenomination = coinMaking.nextDouble();
+		GBPMint GBP = GBPMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
+		GBP.makeCoin();
+	    }
+	    if (!counterfeitCoinCountry.equals("0")) coinMaking.nextLine();
+	} while (!counterfeitCoinCountry.equals("0"));
+    }
 
 }
