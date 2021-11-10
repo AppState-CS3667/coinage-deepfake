915b1645cef0d547dfae0f2c9d0df83ceae28bd2
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:22:04 -0500

    fixed indentation

--- src/CompT.java
+++ src/CompT.java
@@ -5,11 +5,11 @@
  * (two pound)
  */
 public class CompT implements smelting{
-    /*
-     * smelting method - displays composition
+	/*
+	 * smelting method - displays composition
 	 * @return void
-     */
-    public void smelt() {
-        System.out.println("Outer Ring:\n76% Cu\n20% Zi\n4% Ni\nInner Ring:\n75% Cu\n25% Ni");
-    }
+	 */
+	public void smelt() {
+		System.out.println("Outer Ring:\n76% Cu\n20% Zi\n4% Ni\nInner Ring:\n75% Cu\n25% Ni");
+	}
 }
