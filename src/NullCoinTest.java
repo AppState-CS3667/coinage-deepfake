9452cf59dbaa3ff4d52fa29220a3bc21e489a45d
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:33:08 -0500

    indentation

--- src/NullCoinTest.java
+++ src/NullCoinTest.java
@@ -14,59 +14,59 @@ public class NullCoinTest {
 	 * Method for testing smelting
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testNullCoin = Coin.NULL;
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "A Null coin can't be smelted\n";
-            String testOutput = null;
-            testNullCoin.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testSmelt() {
+			Coin testNullCoin = Coin.NULL;
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "A Null coin can't be smelted\n";
+			String testOutput = null;
+			testNullCoin.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Method for testing denomination
 	 * @return void
 	 */
-    @Test
-        public void testgetDenomination() {
-            Coin testNullCoin = Coin.NULL;
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            Double expectedResult = 0.0;
-            Double testOutput = testNullCoin.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testgetDenomination() {
+			Coin testNullCoin = Coin.NULL;
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			Double expectedResult = 0.0;
+			Double testOutput = testNullCoin.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Method for testing case for creation of null coin
 	 * @return void
 	 */
-    @Test
-        public void display() {
-            Coin testNullCoin = Coin.NULL; 
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Incompatable denomination Null coin created\n"; 
-            String testOutput = null;
-            testNullCoin.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void display() {
+			Coin testNullCoin = Coin.NULL; 
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Incompatable denomination Null coin created\n"; 
+			String testOutput = null;
+			testNullCoin.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
