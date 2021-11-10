917f7a18161e0b2790fc3fbfec3322033e119f53
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:40:56 -0500

    indentation

--- src/USDPenny.java
+++ src/USDPenny.java
@@ -4,20 +4,20 @@
  * @version 1.0
  */
 public class USDPenny extends Coin {
-    /*
-     * Coin constructor
-     * @param double denomination
-     * @param String countryCode
-     * @param smelting composition
-     */
-    public USDPenny(double denomination, String countryCode, smelting composition) {
-        super(denomination, countryCode, composition);
-    }
-    /*
-     * Method to run minting message
+	/*
+	 * Coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 */
+	public USDPenny(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+	/*
+	 * Method to run minting message
 	 * @return void
-     */
-    public void display () {
-        System.out.println("Minting a USD Penny!");
-    }
+	 */
+	public void display () {
+		System.out.println("Minting a USD Penny!");
+	}
 }
