c68fa224269bd5d13b5f58e5139ac8a137d6fcc4
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:29:18 -0500

    indentation

--- src/GBPPoundTest.java
+++ src/GBPPoundTest.java
@@ -14,69 +14,69 @@ public class GBPPoundTest {
 	 * Constructor for test class
 	 * @return void
 	 */
-    @Test
-        public void testConstructor() {
-            try {
-                Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
-                assertNotNull(testGBPPound);
-            }
-            catch (Exception e) {
-                fail("Constructor threw exception: " + e.getMessage());
-            }
-        }
+	@Test
+		public void testConstructor() {
+			try {
+				Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
+				assertNotNull(testGBPPound);
+			}
+			catch (Exception e) {
+				fail("Constructor threw exception: " + e.getMessage());
+			}
+		}
 	/*
 	 * Function to run smelting test
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Beginning smelting:\n70% Cu\n24.5% Zi\n5.5% Ni\n"; 
-            String testOutput = null;
-            testGBPPound.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testSmelt() {
+			Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Beginning smelting:\n70% Cu\n24.5% Zi\n5.5% Ni\n"; 
+			String testOutput = null;
+			testGBPPound.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run minting test
 	 * @return void
 	 */
-    @Test
-        public void display() {
-            Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Minting a GBP Pound!\n"; 
-            String testOutput = null;
-            testGBPPound.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void display() {
+			Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Minting a GBP Pound!\n"; 
+			String testOutput = null;
+			testGBPPound.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run test on coin denomination
 	 * @return void
 	 */
-    @Test
-        public void testgetDenomination() {
-            Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
-            Double expectedResult = 1.0;
-            Double testOutput = testGBPPound.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testgetDenomination() {
+			Coin testGBPPound = new GBPPound(1, "GBP", new CompPo());
+			Double expectedResult = 1.0;
+			Double testOutput = testGBPPound.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
