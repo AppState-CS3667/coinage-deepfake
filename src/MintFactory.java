841742b9945c3dd3fdf9d7ec020486f33afce8c3
loganwrichardson <logan.richardson90@gmail.com>
2021-11-10 18:25:58 -0500

    indent

--- src/MintFactory.java
+++ src/MintFactory.java
@@ -5,18 +5,18 @@ import java.util.Random;
  * @version 1.0
  */
 public abstract class MintFactory {
-	/*
-	 * @fields
-	 * double denom - denomination
-	 * String cc - country code
-	 * Random rand - random object
-	 */
-	protected double denom = 0;
-	protected String cc = "";
-	protected Random rand = new Random();
-	/*
-	 * abstract makeCoin method
-	 * @return Coin
-	 */
-	abstract Coin makeCoin();
+    /*
+     * @fields
+     * double denom - denomination
+     * String cc - country code
+     * Random rand - random object
+     */
+    protected double denom = 0;
+    protected String cc = "";
+    protected Random rand = new Random();
+    /*
+     * abstract makeCoin method
+     * @return Coin
+     */
+    abstract Coin makeCoin();
 }
