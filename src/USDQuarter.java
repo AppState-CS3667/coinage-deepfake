4c00c6b8476a51a3697f57ffbeeb52b785e812f6
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:41:46 -0500

    indentation

--- src/USDQuarter.java
+++ src/USDQuarter.java
@@ -4,20 +4,20 @@
  * @version 1.0
  */
 public class USDQuarter extends Coin {
-    /*
-     * Coin constructor
-     * @param double denomination
-     * @param String countryCode
-     * @param smelting composition
-     */
-    public USDQuarter(double denomination, String countryCode, smelting composition) {
-        super(denomination, countryCode, composition);
-    }
-    /*
-     * Method to run minting message
+	/*
+	 * Coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 */
+	public USDQuarter(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+	/*
+	 * Method to run minting message
 	 * @return void
-     */
-    public void display () {
-        System.out.println("Minting a USD Quarter!");
-    }
+	 */
+	public void display () {
+		System.out.println("Minting a USD Quarter!");
+	}
 }
