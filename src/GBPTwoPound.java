996cecaf2f7d15a95a7ebcbdba17807d607e45ea
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:32:12 -0500

    indentation

--- src/GBPTwoPound.java
+++ src/GBPTwoPound.java
@@ -4,20 +4,20 @@
  * @version 1.0
  */
 public class GBPTwoPound extends Coin {
-    /*
-     * Coin constructor
-     * @param double denomination
-     * @param String countryCode
-     * @param smelting composition
-     */
-    public GBPTwoPound(double denomination, String countryCode, smelting composition) {
-        super(denomination, countryCode, composition);
-    }
-    /*
-     * Method to run minting message
+	/*
+	 * Coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 */
+	public GBPTwoPound(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+	/*
+	 * Method to run minting message
 	 * @return void
-     */
-    public void display () {
-        System.out.println("Minting a GBP TwoPound!");
-    }
+	 */
+	public void display () {
+		System.out.println("Minting a GBP TwoPound!");
+	}
 }
