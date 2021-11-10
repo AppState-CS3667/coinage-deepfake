49d21d5d8977ab3464083d1672c4ddb2a3a5bc7b
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:42:21 -0500

    indentation

--- src/USDQuarterTest.java
+++ src/USDQuarterTest.java
@@ -15,68 +15,68 @@ public class USDQuarterTest {
 	 * @return void
 	 */
 	@Test
-        public void testConstructor() {
-            try {
-                Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
-                assertNotNull(testUSDQuarter);
-            }
-            catch (Exception e) {
-                fail("Constructor threw exception: " + e.getMessage());
-            }
-        }
+		public void testConstructor() {
+			try {
+				Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
+				assertNotNull(testUSDQuarter);
+			}
+			catch (Exception e) {
+				fail("Constructor threw exception: " + e.getMessage());
+			}
+		}
 	/*
 	 * Function to run smelting test
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Beginning smelting:\n91.67% Cu\n8.33% Ni\n";; 
-            String testOutput = null;
-            testUSDQuarter.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
-    /*
+	@Test
+		public void testSmelt() {
+			Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Beginning smelting:\n91.67% Cu\n8.33% Ni\n";; 
+			String testOutput = null;
+			testUSDQuarter.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
+	/*
 	 * Function to run minting test
 	 * @return void
 	 */
 	@Test
-        public void display() {
-            Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Minting a USD Quarter!\n"; 
-            String testOutput = null;
-            testUSDQuarter.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
-    /*
+		public void display() {
+			Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Minting a USD Quarter!\n"; 
+			String testOutput = null;
+			testUSDQuarter.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
+	/*
 	 * Function to run test on coin denomination
 	 * @return void
 	 */
 	@Test
-        public void testgetDenomination() {
-            Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
-            Double expectedResult = 0.25;
-            Double testOutput = testUSDQuarter.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+		public void testgetDenomination() {
+			Coin testUSDQuarter = new USDQuarter(0.25,"USD",new CompHQD());
+			Double expectedResult = 0.25;
+			Double testOutput = testUSDQuarter.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
