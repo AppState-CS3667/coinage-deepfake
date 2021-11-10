7dc276f3d5544c1bb9cad7ec6c8dbac25f30682e
loganwrichardson <logan.richardson90@gmail.com>
2021-11-10 10:19:16 -0500

    indentation

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -10,70 +10,70 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  */
 public class CoinTest extends Coin {
-	/*
-	 * Constructor for test class
-	 * @return void
-	 */
-	public CoinTest() {
-		super(0.05, "USD", new CompNFFTF());
-	}
-	/*
-	 * Function to run constructor test
-	 * @return void
-	 */
-	@Test
-		public void testConstructor() {
-			try {
-				Coin testCoin = new CoinTest();
-				assertNotNull(testCoin);
-			}
-			catch (Exception e) {
-				fail("Constructor threw exception: " + e.getMessage());
-			}
-		}
-	/*
-	 * Function to run smelting test
-	 * @return void
-	 */
-	@Test
-		public void testSmelt() {
-			Coin testCoin = new CoinTest();
-			PrintStream origOut = System.out;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			PrintStream newOut = new PrintStream(baos);
-			System.setOut(newOut);
-			String expectedResult = "Beginning smelting:\n75% Cu\n25% Ni\n";
-			String testOutput = null;
-			testCoin.smelt();
-			System.out.flush();
-			testOutput = baos.toString();
-			System.setOut(origOut);
-			assertTrue(expectedResult.equals(testOutput),
-					"Expected:'" + expectedResult 
-					+ "' but got '" + testOutput + "'.");
-		}
-	/*
-	 * Function to run test on coin denomination
-	 * @return void
-	 */
-	@Test
-		public void testgetDenomination() {
-			Coin testCoin = new CoinTest();
-			PrintStream origOut = System.out;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			PrintStream newOut = new PrintStream(baos);
-			System.setOut(newOut);
-			Double expectedResult = 0.05;
-			Double testOutput = testCoin.getDenomination();
-			assertEquals(expectedResult, testOutput,
-					"Expected:'" + expectedResult 
-					+ "' but got '" + testOutput + "'.");
-		}
-	/*
-	 * display method
-	 * @return void
-	 */
-	public void display() {
-		return;
-	}
+    /*
+     * Constructor for test class
+     * @return void
+     */
+    public CoinTest() {
+        super(0.05, "USD", new CompNFFTF());
+    }
+    /*
+     * Function to run constructor test
+     * @return void
+     */
+    @Test
+        public void testConstructor() {
+            try {
+                Coin testCoin = new CoinTest();
+                assertNotNull(testCoin);
+            }
+            catch (Exception e) {
+                fail("Constructor threw exception: " + e.getMessage());
+            }
+        }
+    /*
+     * Function to run smelting test
+     * @return void
+     */
+    @Test
+        public void testSmelt() {
+            Coin testCoin = new CoinTest();
+            PrintStream origOut = System.out;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream newOut = new PrintStream(baos);
+            System.setOut(newOut);
+            String expectedResult = "Beginning smelting:\n75% Cu\n25% Ni\n";
+            String testOutput = null;
+            testCoin.smelt();
+            System.out.flush();
+            testOutput = baos.toString();
+            System.setOut(origOut);
+            assertTrue(expectedResult.equals(testOutput),
+                    "Expected:'" + expectedResult 
+                    + "' but got '" + testOutput + "'.");
+        }
+    /*
+     * Function to run test on coin denomination
+     * @return void
+     */
+    @Test
+        public void testgetDenomination() {
+            Coin testCoin = new CoinTest();
+            PrintStream origOut = System.out;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream newOut = new PrintStream(baos);
+            System.setOut(newOut);
+            Double expectedResult = 0.05;
+            Double testOutput = testCoin.getDenomination();
+            assertEquals(expectedResult, testOutput,
+                    "Expected:'" + expectedResult 
+                    + "' but got '" + testOutput + "'.");
+        }
+    /*
+     * display method
+     * @return void
+     */
+    public void display() {
+        return;
+    }
 }
