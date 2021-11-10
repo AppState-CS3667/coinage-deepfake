a80ac8a6cd1191ba63947408bbcca6c21cba9dc9
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:18:25 -0500

    fixed indentation

--- src/CompD.java
+++ src/CompD.java
@@ -5,11 +5,11 @@
  * (dollar)
  */
 public class CompD implements smelting{
-    /*
-     * smelting method - displays composition
+	/*
+	 * smelting method - displays composition
 	 * @return void
-     */
-    public void smelt() {
-        System.out.println("88.5% Cu\n6% Zi\n3.5% Mn\n2% Ni");
-    }
+	 */
+	public void smelt() {
+		System.out.println("88.5% Cu\n6% Zi\n3.5% Mn\n2% Ni");
+	}
 }
