3783cdb136415e8b515df7040fc6ac020d029e4e
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:35:11 -0500

    indentation

--- src/USDDime.java
+++ src/USDDime.java
@@ -4,20 +4,20 @@
  * @version 1.0
  */
 public class USDDime extends Coin {
-    /*
-     * Coin constructor
-     * @param double denomination
-     * @param String countryCode
-     * @param smelting composition
-     */
-    public USDDime(double denomination, String countryCode, smelting composition) {
-        super(denomination, countryCode, composition);
-    }
-    /*
-     * Method to run minting message
+	/*
+	 * Coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 */
+	public USDDime(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+	/*
+	 * Method to run minting message
 	 * @return void
-     */
-    public void display () {
-        System.out.println("Minting a USD Dime!");
-    }
+	 */
+	public void display () {
+		System.out.println("Minting a USD Dime!");
+	}
 }
