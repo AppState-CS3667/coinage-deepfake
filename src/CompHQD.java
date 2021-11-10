1dd8ccbc65584bd4c04780d8b76770ec800bdbb0
loganwrichardson <logan.richardson90@gmail.com>
2021-11-10 18:24:47 -0500

    indentation

--- src/CompHQD.java
+++ src/CompHQD.java
@@ -5,11 +5,11 @@
  * (half dollar, quarter, dime)
  */
 public class CompHQD implements smelting{
-	/*
-	 * smelting method - displays composition
-	 * @return void
-	 */
-	public void smelt() {
-		System.out.println("91.67% Cu\n8.33% Ni");
-	}
+    /*
+     * smelting method - displays composition
+     * @return void
+     */
+    public void smelt() {
+        System.out.println("91.67% Cu\n8.33% Ni");
+    }
 }
