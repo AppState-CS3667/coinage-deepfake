36f317be5879dcbc97b87628fd42627fabe43951
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:30:26 -0500

    indentation

--- src/GBPTwentyPence.java
+++ src/GBPTwentyPence.java
@@ -4,20 +4,20 @@
  * @version 1.0
  */
 public class GBPTwentyPence extends Coin {
-    /*
-     * Coin constructor
-     * @param double denomination
-     * @param String countryCode
-     * @param smelting composition
-     */
-    public GBPTwentyPence(double denomination, String countryCode, smelting composition) {
-        super(denomination, countryCode, composition);
-    }
-    /*
-     * Method to run minting message
+	/*
+	 * Coin constructor
+	 * @param double denomination
+	 * @param String countryCode
+	 * @param smelting composition
+	 */
+	public GBPTwentyPence(double denomination, String countryCode, smelting composition) {
+		super(denomination, countryCode, composition);
+	}
+	/*
+	 * Method to run minting message
 	 * @return void
-     */
-    public void display () {
-        System.out.println("Minting a GBP TwentyPence!");
-    }
+	 */
+	public void display () {
+		System.out.println("Minting a GBP TwentyPence!");
+	}
 }
