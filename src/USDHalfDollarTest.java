a410648f72e92c4d8330b89cbabb5e514140f21a
loganwrichardson <logan.richardson90@gmail.com>
2021-11-09 22:38:39 -0500

    indentation

--- src/USDHalfDollarTest.java
+++ src/USDHalfDollarTest.java
@@ -10,73 +10,73 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  */
 public class USDHalfDollarTest {
-    /*
+	/*
 	 * Constructor for test class
 	 * @return void
 	 */
 	@Test
-        public void testConstructor() {
-            try {
-                Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
-                assertNotNull(testUSDHalfDollar);
-            }
-            catch (Exception e) {
-                fail("Constructor threw exception: " + e.getMessage());
-            }
-        }
+		public void testConstructor() {
+			try {
+				Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
+				assertNotNull(testUSDHalfDollar);
+			}
+			catch (Exception e) {
+				fail("Constructor threw exception: " + e.getMessage());
+			}
+		}
 	/*
 	 * Function to run smelting test
 	 * @return void
 	 */
-    @Test
-        public void testSmelt() {
-            Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Beginning smelting:\n91.67% Cu\n8.33% Ni\n"; 
-            String testOutput = null;
-            testUSDHalfDollar.smelt();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testSmelt() {
+			Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Beginning smelting:\n91.67% Cu\n8.33% Ni\n"; 
+			String testOutput = null;
+			testUSDHalfDollar.smelt();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run minting test
 	 * @return void
 	 */
-    @Test
-        public void display() {
-            Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
-            PrintStream origOut = System.out;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            PrintStream newOut = new PrintStream(baos);
-            System.setOut(newOut);
-            String expectedResult = "Minting a USD HalfDollar!\n"; 
-            String testOutput = null;
-            testUSDHalfDollar.display();
-            System.out.flush();
-            testOutput = baos.toString();
-            System.setOut(origOut);
-            assertTrue(expectedResult.equals(testOutput),
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void display() {
+			Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
+			PrintStream origOut = System.out;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream newOut = new PrintStream(baos);
+			System.setOut(newOut);
+			String expectedResult = "Minting a USD HalfDollar!\n"; 
+			String testOutput = null;
+			testUSDHalfDollar.display();
+			System.out.flush();
+			testOutput = baos.toString();
+			System.setOut(origOut);
+			assertTrue(expectedResult.equals(testOutput),
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 	/*
 	 * Function to run test on coin denomination
 	 * @return void
 	 */
-    @Test
-        public void testgetDenomination() {
-            Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
-            Double expectedResult = 0.5;
-            Double testOutput = testUSDHalfDollar.getDenomination();
-            assertEquals(expectedResult, testOutput,
-                    "Expected:'" + expectedResult 
-                    + "' but got '" + testOutput + "'.");
-        }
+	@Test
+		public void testgetDenomination() {
+			Coin testUSDHalfDollar = new USDHalfDollar(0.5,"USD",new CompHQD());
+			Double expectedResult = 0.5;
+			Double testOutput = testUSDHalfDollar.getDenomination();
+			assertEquals(expectedResult, testOutput,
+					"Expected:'" + expectedResult 
+					+ "' but got '" + testOutput + "'.");
+		}
 }
