f04c5c0a30ca7e2d12cd1da36945fa55aedd57a1
IsaacMAllen <allenim@appstate.edu>
2021-11-10 20:07:52 -0500

    finished up demo

--- Demo.java
+++ Demo.java
@@ -16,34 +16,44 @@ public class Demo {
 	System.out.println("What coin would you like to make?");
 	do {
 	    if (!runOnce) {
-		System.out.println("Which countries coin do you want to make? Enter USD or GBP (0 to exit):");
+		System.out.println("Which countries coin do you want to make? Enter USD or GBP (0 to exit, Defaults to USD):");
 		runOnce = true;
 	    } else {
-		System.out.println("Want to make another coin? Enter USD or GBP (0 to exit):");
+		System.out.println("Want to make another coin? Enter USD or GBP (0 to exit, Defaults to USD):");
 	    }
 	    counterfeitCoinCountry = coinMaking.nextLine();
 	    counterfeitCoinCountry = counterfeitCoinCountry.toUpperCase();
+	    if (counterfeitCoinCountry.equals("")) counterfeitCoinCountry = "USD";
 	    while(((!counterfeitCoinCountry.equals("GBP")) && (!counterfeitCoinCountry.equals("USD"))) && (!counterfeitCoinCountry.equals("0"))) {
-		System.out.println("We can't make that type of coin. \n Try again.");
+		System.out.println("We can't make that type of coin, try again.");
+		System.out.println("Enter USD or GBP (0 to exit, Defaults to USD):");
 		counterfeitCoinCountry = coinMaking.nextLine();
 		counterfeitCoinCountry = counterfeitCoinCountry.toUpperCase();
+		if (counterfeitCoinCountry.equals("")) counterfeitCoinCountry = "USD";
 	    }
 
 	    if(counterfeitCoinCountry.equals("USD")) {
 		System.out.println("You want to make a USD Coin.");
 		System.out.println("What denomination do you want to make?");
-		counterfeitCoinDenomination = coinMaking.nextDouble();
+		try {
+		    counterfeitCoinDenomination = Double.parseDouble(coinMaking.nextLine());
+		} catch (NumberFormatException e) {
+		    counterfeitCoinDenomination = 100;
+		}
 		USDMint USD = USDMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
 		USD.makeCoin();
 	    }
 	    else if(counterfeitCoinCountry.equals("GBP")) {
 		System.out.println("You want to make a GBP Coin.");
 		System.out.println("What denomination do you want to make?");
-		counterfeitCoinDenomination = coinMaking.nextDouble();
+		try {
+		    counterfeitCoinDenomination = Double.parseDouble(coinMaking.nextLine());
+		} catch (NumberFormatException e) {
+		    counterfeitCoinDenomination = 100;
+		}
 		GBPMint GBP = GBPMint.getInstance(counterfeitCoinDenomination, counterfeitCoinCountry);
 		GBP.makeCoin();
 	    }
-	    if (!counterfeitCoinCountry.equals("0")) coinMaking.nextLine();
 	} while (!counterfeitCoinCountry.equals("0"));
     }
 
